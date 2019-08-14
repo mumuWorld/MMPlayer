d9b1a9135bc778e5c80ec71970b482fad7e7bf50
杨杰 <jieyangb@ctrip.com>
2019-08-14 21:47:32 +0800

    -功能[ first ] -备注[ ]

---
+++ MMPlayer/MMPlayer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MMPlayer
+//
+//  Created by mumu on 2019/8/13.
+//  Copyright © 2019 Mumu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

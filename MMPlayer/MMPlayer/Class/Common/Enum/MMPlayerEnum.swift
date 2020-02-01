9fb1a189bf49e5564398bc522981b01ecfea5f15
杨杰 <617958070@qq.com>
2020-02-01 10:33:35 +0800

    -功能[ 未知 ] -备注[ ]

---
+++ MMPlayer/MMPlayer/Class/Common/Enum/MMPlayerEnum.swift
@@ -0,0 +1,14 @@
+//
+//  MMPlayerEnum.swift
+//  MMPlayer
+//
+//  Created by mumu on 2019/9/7.
+//  Copyright © 2019 Mumu. All rights reserved.
+//
+
+import Foundation
+
+enum MMPlayerMediaType {
+    case music
+    case video
+}
---
+++ MMPlayer/MMPlayer/Class/Main/Model/MMPlayerModel.swift
@@ -0,0 +1,15 @@
+//
+//  MMPlayerModel.swift
+//  MMPlayer
+//
+//  Created by mumu on 2019/9/7.
+//  Copyright © 2019 Mumu. All rights reserved.
+//
+
+import Foundation
+
+struct MMAVItem {
+    var name: String?
+    var filePath: String?
+    var mediaType: MMPlayerMediaType = .music
+}
